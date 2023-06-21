6f8aaceb7530aed5f16d3ceb7e092bb90a38ff43
miroNever <123352959+miroNever@users.noreply.github.com>
2023-06-21 18:33:32 +0500

    Add files via upload

---
+++ zd6/zd6/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace zd6
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage(string Login, string Password)
+        {
+            InitializeComponent();
+            login.Text = $"Ваш логин: {Login}";
+            password.Text = $"Ваш пароль: {Password}";
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (swich1.IsToggled == true)
+            {
+                OnOff1.Text = "ON";
+            }
+            else
+                OnOff1.Text = "OFF";
+        }
+
+        private void swich2_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (swich2.IsToggled == true)
+            {
+                OnOff2.Text = "ON";
+            }
+            else
+                OnOff2.Text = "OFF";
+        }
+
+        private void Switch_Toggled_1(object sender, ToggledEventArgs e)
+        {
+            if (swich3.IsToggled == true)
+            {
+                OnOff3.Text = "✓";
+            }
+            else
+                OnOff3.Text = "✕";
+        }
+
+        private void swich4_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (swich4.IsToggled == true)
+            {
+                OnOff4.Text = "✓";
+            }
+            else
+                OnOff4.Text = "✕";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double[] array = new double[4]{ Convert.ToDouble(slider1.Value), Convert.ToDouble(slider2.Value), Convert.ToDouble(slider3.Value), Convert.ToDouble(slider4.Value)};
+            double Max = array.Max();
+            MaxVlue.Text = $"Максимальное значение Slider'ов: {Max}";
+            login.IsVisible = true;
+            password.IsVisible = true;
+        }
+
+    }
+}
---
+++ zd6/zd6/Welcome.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zd6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Welcome : ContentPage
+    {
+        static string Login;
+        static string Password;
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+        private bool CheckFields(string login, string password)
+        {
+            if (login == null || login.Length <= 2)
+            {
+                this.DisplayToastAsync("Длина логина должна быть больше 2 символов");
+
+                return false;
+            }
+            if (password == null || password.Length <= 4)
+            {
+                this.DisplayToastAsync("Длина пароля должна быть больше 4 символов");
+                return false;
+            }
+            Login = login;
+            Password = password;
+            return true;
+        }
+        private void singInButton_Clicked(object sender, EventArgs e)
+        {
+            if (CheckFields(login.Text, password.Text))
+            {
+                Navigation.PushAsync(new MainPage(Login, Password));
+            }
+        }
+    }
+}
